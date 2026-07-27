c8744a0f0d53c736c23036a7c7c63c7e3f09d9b6
jodgha <98224241+jodgha@users.noreply.github.com>
2026-07-27 19:22:55 +0800

    Create cmake_clean.cmake

---
+++ build/vehicle_4axle_8steer/CMakeFiles/vehicle_4axle_8steer_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vehicle_4axle_8steer_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/vehicle_4axle_8steer_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
